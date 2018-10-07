a6bfa21bd95dd6884c7a166ca3857fc4bb41c74e
Billy Wolfington <wolfington98@gmail.com>
2018-10-07 00:59:12 -0700

    Start to building DI extensions library

---
+++ src/ArgentPonyWarcraftClient.Extensions.DependencyInjection/WarcraftClientServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Options;
+
+namespace ArgentPonyWarcraftClient.Extensions.DependencyInjection
+{
+    public static class WarcraftClientServiceCollectionExtensions
+    {
+        public static IServiceCollection UseWarcraftClient(this IServiceCollection services)
+        {
+            services.AddOptions<WarcraftClientOptions>();
+            services.AddScoped<WarcraftClientOptions>(s => s.GetService<IOptions<WarcraftClientOptions>>().Value);
+
+            services.AddHttpClient<IWarcraftClient, WarcraftClient>()
+                .ConfigureHttpClient(client =>
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                });
+
+            return services;
+        }
+    }
+}
